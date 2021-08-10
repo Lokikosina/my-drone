869c1e9bc0afe1b8a751244f5b8a6e10e453c512
kurisu20 <Creator@DESKTOP-QNRTKVC>
2021-08-10 15:42:03 +0200

    sere me

---
+++ src/main/java/net/mcreator/mydrone/block/SonicmetalblockBlock.java
@@ -0,0 +1,49 @@
+
+package net.mcreator.mydrone.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.mydrone.MyDroneModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@MyDroneModElements.ModElement.Tag
+public class SonicmetalblockBlock extends MyDroneModElements.ModElement {
+	@ObjectHolder("my_drone:sonicmetalblock")
+	public static final Block block = null;
+	public SonicmetalblockBlock(MyDroneModElements instance) {
+		super(instance, 12);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0));
+			setRegistryName("sonicmetalblock");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/mydrone/enchantment/SonicEnchantment.java
@@ -0,0 +1,60 @@
+
+package net.mcreator.mydrone.enchantment;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.util.DamageSource;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.enchantment.EnchantmentType;
+import net.minecraft.enchantment.Enchantment;
+
+import net.mcreator.mydrone.MyDroneModElements;
+
+@MyDroneModElements.ModElement.Tag
+public class SonicEnchantment extends MyDroneModElements.ModElement {
+	@ObjectHolder("my_drone:sonic")
+	public static final Enchantment enchantment = null;
+	public SonicEnchantment(MyDroneModElements instance) {
+		super(instance, 8);
+	}
+
+	@Override
+	public void initElements() {
+		elements.enchantments.add(() -> new CustomEnchantment(EquipmentSlotType.MAINHAND).setRegistryName("sonic"));
+	}
+	public static class CustomEnchantment extends Enchantment {
+		public CustomEnchantment(EquipmentSlotType... slots) {
+			super(Enchantment.Rarity.COMMON, EnchantmentType.WEAPON, slots);
+		}
+
+		@Override
+		public int getMinLevel() {
+			return 1;
+		}
+
+		@Override
+		public int getMaxLevel() {
+			return 10;
+		}
+
+		@Override
+		public int calcModifierDamage(int level, DamageSource source) {
+			return level * 45;
+		}
+
+		@Override
+		public boolean isTreasureEnchantment() {
+			return false;
+		}
+
+		@Override
+		public boolean isCurse() {
+			return false;
+		}
+
+		@Override
+		public boolean isAllowedOnBooks() {
+			return true;
+		}
+	}
+}
